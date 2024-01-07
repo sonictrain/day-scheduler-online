d46b3d6fd116cd3574af5b96eae4044e1ee62ec5
sonictrain <nicola.brucoli92@gmail.com>
2024-01-07 01:33:15 +0100

    added function to generate rows in the agends, plus added flex horizontal property to the .row class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 // HTML element variables
 const currentDayEl = document.getElementById("currentDay");
-const agendaContainer = document.querySelector(".container");
 
 // Global Day object
 let currentDay = {
     day: dayjs().format("dddd"),
     dayNum: Number(dayjs().format("D")),
     month: dayjs().format("MMMM"),
+    monthNum: dayjs().format("M"),
     year: Number(dayjs().format("YYYY")),
     hours: Number(dayjs().format("H")),
     minutes: Number(dayjs().format("m")),
     seconds: Number(dayjs().format("s")),
 }
+
+let workDay = {
+    start: 9,
+    end: 18,
+}
+
+createTimeSlots();
 
 // Timed function to update date & time every second
 setInterval(() => {
@@ -29,7 +36,7 @@ setInterval(() => {
 
     // Update sup tag
     ordinalTag();
-    
+
     // Render date and sup tag  
     $(currentDayEl).text(`${currentDay.day}, ${currentDay.month} ${currentDay.dayNum}`).append($('<sup>').text(sup));
 
@@ -56,5 +63,33 @@ function ordinalTag() {
             sup = "th";
             break;
     }
-    
+
+}
+
+// Create and Render Time Slots for the current work day
+function createTimeSlots() {
+    for (let i = workDay.start; i <= workDay.end; i++) {
+
+        const agendaContainer = document.querySelector(".container");
+        let row = $('<div>').addClass('row');
+        
+        let date = new Date(currentDay.year, currentDay.monthNum, currentDay.dayNum, i)
+        let timeBlockLabel = dayjs(date).format('h A');
+        let timeBlock = $('<div>').addClass('time-block').text(timeBlockLabel);
+
+        let description = $('<div>').addClass('description');
+        let saveBtn = $('<div>').addClass('saveBtn');
+
+        $(agendaContainer)
+        .append(row);
+
+        $(row)
+        .append(timeBlock)
+        .append(description)
+        .append(saveBtn);
+
+        $(saveBtn)
+        .append($('<i>').addClass('far fa-save'));
+
+    }
 }
