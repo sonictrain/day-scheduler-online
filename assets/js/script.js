646b3e3d5271326d7df1ddc82a18db34078bffb8
sonictrain <nicola.brucoli92@gmail.com>
2024-01-08 15:22:57 +0100

    added checks object to update the DOM when necessary only, added color code to the row using jquery, added eventListener to grab event data (time and description)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ let currentDay = {
     day: dayjs().format("dddd"),
     dayNum: Number(dayjs().format("D")),
     month: dayjs().format("MMMM"),
-    monthNum: dayjs().format("M"),
+    monthNum: Number(dayjs().format("M")),
     year: Number(dayjs().format("YYYY")),
     hours: Number(dayjs().format("H")),
     minutes: Number(dayjs().format("m")),
@@ -18,6 +18,13 @@ let workDay = {
     end: 18,
 }
 
+let checks = {
+    day: 0,
+    hours: 0,
+}
+
+let eventsObj = []
+
 createTimeSlots();
 
 // Timed function to update date & time every second
@@ -28,17 +35,36 @@ setInterval(() => {
         day: dayjs().format("dddd"),
         dayNum: Number(dayjs().format("D")),
         month: dayjs().format("MMMM"),
+        monthNum: dayjs().format("M"),
         year: Number(dayjs().format("YYYY")),
         hours: Number(dayjs().format("H")),
         minutes: Number(dayjs().format("m")),
         seconds: Number(dayjs().format("s")),
     }
 
-    // Update sup tag
-    ordinalTag();
+    // only update the DOM if the day is changed
+    if (checks.day !== currentDay.dayNum) {
 
-    // Render date and sup tag  
-    $(currentDayEl).text(`${currentDay.day}, ${currentDay.month} ${currentDay.dayNum}`).append($('<sup>').text(sup));
+        // Update sup tag
+        ordinalTag();
+
+        // Render date and sup tag  
+        $(currentDayEl).text(`${currentDay.day}, ${currentDay.month} ${currentDay.dayNum}`).append($('<sup>').text(sup));
+
+    };
+
+    // refresh DOM (rows) every hour
+    if (checks.hours !== currentDay.hours) {
+
+        // Update rows
+        createTimeSlots();
+
+    }
+
+    checks = {
+        day: currentDay.dayNum,
+        hours: currentDay.hours,
+    }
 
 }, 1000);
 
@@ -68,28 +94,62 @@ function ordinalTag() {
 
 // Create and Render Time Slots for the current work day
 function createTimeSlots() {
+
+    const agendaContainer = document.querySelector(".container");
+    // clear the container everytime the function runs
+    $(agendaContainer).empty();
+
     for (let i = workDay.start; i <= workDay.end; i++) {
 
-        const agendaContainer = document.querySelector(".container");
         let row = $('<div>').addClass('row');
-        
+
+        // convert i value to AM/PM time and add label to the timeblock div-- eg: convert 9 to 9AM
         let date = new Date(currentDay.year, currentDay.monthNum, currentDay.dayNum, i)
         let timeBlockLabel = dayjs(date).format('h A');
         let timeBlock = $('<div>').addClass('time-block').text(timeBlockLabel);
 
-        let description = $('<div>').addClass('description');
-        let saveBtn = $('<div>').addClass('saveBtn');
+        // create description div and style accordingly
+        let description = $('<input>').addClass('description');
 
+        if (currentDay.hours < i) {
+            description.addClass('future');
+        } else if (currentDay.hours == i) {
+            description.addClass('present');
+        } else {
+            description.addClass('past');
+        }
+
+        // create saveBtn
+        let saveBtn = $('<div>').addClass('saveBtn');
+        let saveIcon = $('<i>').addClass('far fa-save');
+
+        saveBtn.append(saveIcon);
+
+        saveIcon.on('click', (e) => {
+
+            // Return if input field is blank
+            if (description.val()) {
+
+                eventsObj.push(
+                    {
+                        hour: $(e.target).parent().siblings('.time-block').text(),
+                        event: $(e.target).parent().siblings('.description').val()
+                    }
+                )
+                saveEvent(eventsObj);
+
+            } else {
+                return;
+            }
+        });
+
+        // append all the elements inside row, and append row inside container
         $(agendaContainer)
-        .append(row);
+            .append(row);
 
         $(row)
-        .append(timeBlock)
-        .append(description)
-        .append(saveBtn);
-
-        $(saveBtn)
-        .append($('<i>').addClass('far fa-save'));
-
+            .append(timeBlock)
+            .append(description)
+            .append(saveBtn);
     }
 }
